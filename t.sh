d32e90d316497408b961fd89d2408ea25b7d4373
yunkyung318 <dbsrud318@naver.com>
2024-07-15 02:55:36 +0000

    v2

--- t.sh
+++
@@ -1,5 +0,0 @@
-kubectl get secret -n test-network org-orderer1-tls-cert -o json \
-    | jq -r '.data["tls.crt"]' \
-    | base64 -d \
-    > build/channel-msp/orderer1Organizations/org/orderers/org-orderer1/tls/signcerts/tls-cert.pem
-
